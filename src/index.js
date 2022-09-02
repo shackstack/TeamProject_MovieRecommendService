1aaeb76ffac4fd773191ec249bedccc86e64868f
whitebsm93 <whitebsm93@naver.com>
2022-09-02 23:20:24 +0900

    Basic Settings
    - redux, react-redux, redux-toolkit add
    - configStore settings

--- src/index.js
+++ src/index.js
@@ -2,8 +2,8 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import "./index.css";
-import { Provider } from "react";
 import store from "./store/config/configStore";
+import { Provider } from "react";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
